7eda6b63aa1a1ffdd1abe18ba32c70b80815c487
Ferdyawan Listanto <ferdylucker@gmail.com>
2022-02-20 15:13:51 +0700

    update test 1

--- app/Http/Controllers/EndUser/Auth.php
+++ app/Http/Controllers/EndUser/Auth.php
@@ -125,7 +125,7 @@ class Auth extends Controller
             return response()->json([
                 'code' => 404,
                 'status'  => 'error',
-                'message' => 'Email not terdaftar!'
+                'message' => 'Email not terdaftar!!!!'
             ],404);
         }
         if ($data->roles_id != '3')  {
