96606316bbdea6f43e0fd1a36347d6c943606c45
bejombah <ferdylucker@gmail.com>
2022-03-05 03:44:50 +0700

    Search Account Feature

--- app/Http/Controllers/EndUser/GameAccountController.php
+++ app/Http/Controllers/EndUser/GameAccountController.php
@@ -79,7 +79,8 @@ class GameAccountController extends Controller
         $sessGame = $request->session()->get('gamedata');
         try{
             $key = explode(" ", request()->get('key'));
-            $gameAccount = DB::table('game_accounts')->where(function ($query) use ($key) {
+            $gameAccount = DB::table('game_accounts')->where('games_id', '=', $sessGame['game']['id'])
+            ->where(function ($query) use ($key) {
                 foreach ($key as $k) {
                     $query->where('id_game_account', 'like', '%' . $k . '%');
                 }
