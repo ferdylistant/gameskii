b19df15878e6a21cb25f19694fa8a9076f23005e
bejombah <ferdylucker@gmail.com>
2022-03-08 11:49:07 +0700

    Update Scrim

--- app/Http/Controllers/EndUser/TeamController.php
+++ app/Http/Controllers/EndUser/TeamController.php
@@ -50,6 +50,9 @@ class TeamController extends Controller
         try{
             $dataTeam = $this->team->join('team_players','team_players.teams_id','=','teams.id')
             ->join('game_accounts','game_accounts.id_game_account','=','team_players.game_accounts_id')
+            ->join('users','users.id','=','game_accounts.users_id')
+            ->join('games','games.id','=','teams.games_id')
+            ->join('ranks','ranks.id','=','users.ranks_id')
             ->where('teams.games_id',$sessGame['game']['id'])->get();
             return response()->json($dataTeam);
             if ($dataTeam->count() == '0') {
