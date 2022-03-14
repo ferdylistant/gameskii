c87bc1d68f131683a4a0aac270fde9eb286a20d7
bejombah <ferdylucker@gmail.com>
2022-03-14 16:36:32 +0700

    Add Eo Tournament

--- app/Models/EoTournament.php
+++ app/Models/EoTournament.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class EoTournament extends Model
+class EoTournament extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    //
+    use Authenticatable, Authorizable, HasApiTokens;
+
+    protected $table = 'tournament_eos';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = [
+        'id',
+        'game_accounts_id',
+        'organization_name',
+        'organization_email',
+        'organization_phone',
+        'provinsi',
+        'kabupaten',
+        'kecamatan',
+        'address',
+        'status',
+        'verified_at'
+    ];
 }
