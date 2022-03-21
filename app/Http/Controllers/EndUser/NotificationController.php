794f23a663f4a1022d0078c601a37ccaaba63a32
bejombah <ferdylucker@gmail.com>
2022-03-21 12:33:48 +0700

    update notification

--- app/Http/Controllers/EndUser/NotificationController.php
+++ app/Http/Controllers/EndUser/NotificationController.php
@@ -63,7 +63,7 @@ class NotificationController extends Controller
     {
         try {
             $user = auth('user')->user()->unreadNotifications;
-            if (!$user) {
+            if ($user->count() < 0) {
                 return response()->json([
                     'status' => 'error',
                     'message' => 'Notification not found'
