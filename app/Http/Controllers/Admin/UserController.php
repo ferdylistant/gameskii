ec236b0e7260d91e55faf9971698718e62aebf1a
bejombah <ferdylucker@gmail.com>
2022-04-13 15:07:47 +0700

    Add Team Controller in Admin

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -25,12 +25,34 @@ class UserController extends Controller
             }
             $admin = $this->user->join('roles', 'users.roles_id', '=', 'roles.id')
             ->where('users.roles_id', '=', '1')
-            ->where('users.roles_id', '=', '2')
+            ->orWhere('users.roles_id', '=', '2')
             ->select('users.*', 'roles.name as roles_name')
             ->get();
+            foreach ($admin as $value) {
+                $result[] = [
+                    'id' => $value->id,
+                    'name' => $value->name,
+                    'email' => $value->email,
+                    'phone' => $value->phone,
+                    'fb' => $value->fb,
+                    'ig' => $value->ig,
+                    'provinsi' => $value->provinsi,
+                    'kabupaten' => $value->kabupaten,
+                    'kecamatan' => $value->kecamatan,
+                    'tgl_lahir' => $value->tgl_lahir,
+                    'avatar' => $value->avatar,
+                    'roles_name' => $value->roles_name,
+                    'is_verified' => $value->is_verified,
+                    'email_verified_at' => $value->email_verified_at,
+                    'ip_address' => $value->ip_address,
+                    'last_login' => $value->last_login,
+                    'created_at' => $value->created_at,
+                    'updated_at' => $value->updated_at
+                ];
+            }
             return response()->json([
                 "status" => "success",
-                "data" => $admin
+                "data" => $result
             ], 200);
         } catch (\Exception $e) {
             return response()->json([
@@ -53,9 +75,38 @@ class UserController extends Controller
             ->where('users.roles_id', '=', '3')
             ->select('users.*', 'roles.name as roles_name')
             ->get();
+            if ($endUser->count() == 0) {
+                return response()->json([
+                    "status" => "error",
+                    "message" => "No data"
+                ], 404);
+            }
+            foreach ($endUser as $value) {
+                $result[] = [
+                    'id' => $value->id,
+                    'name' => $value->name,
+                    'email' => $value->email,
+                    'phone' => $value->phone,
+                    'fb' => $value->fb,
+                    'ig' => $value->ig,
+                    'provinsi' => $value->provinsi,
+                    'kabupaten' => $value->kabupaten,
+                    'kecamatan' => $value->kecamatan,
+                    'tgl_lahir' => $value->tgl_lahir,
+                    'avatar' => $value->avatar,
+                    'roles_name' => $value->roles_name,
+                    'is_verified' => $value->is_verified,
+                    'email_verified_at' => $value->email_verified_at,
+                    'ip_address' => $value->ip_address,
+                    'last_login' => $value->last_login,
+                    'created_at' => $value->created_at,
+                    'updated_at' => $value->updated_at
+                ];
+            }
             return response()->json([
                 "status" => "success",
-                "data" => $endUser
+                "message" => "Get data success",
+                "data" => $result
             ], 200);
         } catch (\Exception $e) {
             return response()->json([
