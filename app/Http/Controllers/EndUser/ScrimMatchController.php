3d5b19501f931e2a173e942e35919376f66df9d7
Ferdyawan Listanto <ferdylucker@gmail.com>
2022-04-26 14:00:26 +0700

    Make Bracket

--- app/Http/Controllers/EndUser/ScrimMatchController.php
+++ app/Http/Controllers/EndUser/ScrimMatchController.php
@@ -861,26 +861,24 @@ class ScrimMatchController extends Controller
             }
 
             $round=0;
-            $participans = $teamMatch;
-            while(count($participans)>1)
+            while(count($teamMatch)>1)
             {
                 $round++;  // Increment our round
                 $tables=array();  // Clear our tables
                 $index=0;
-                while(count($tables) < floor(count($participans)/2))  // want an even amount of tables
-                    $tables[]=array($participans[$index++],$participans[$index++]);
-                if($index<count($participans)){// extra team, add to tables, but no opposing team
-                    $tables[]=array($participans[$index++],null);
-                }
-                $participans=array(); // clear out next round participants
+                while(count($tables) < floor(count($teamMatch)/2))  // want an even amount of tables
+                    $tables[]=array($teamMatch[$index++],$teamMatch[$index++]);
+                // if($index<count($teamMatch)){// extra team, add to tables, but no opposing team
+                //     $tables[]=array($teamMatch[$index++],null);
+                // }
+                $teamMatch=array(); // clear out next round participants
                 foreach($tables as $idx=>$table)
                 {
                     $tbl=$idx+1;
-                    $participans[] =" Table #{$tbl}: ";
                     if($table[1]===NULL)  // extra team advances to next level automatically
                     {
-                        $participans[] = [
-                            // 'id_scrim' => $table[0]['scrims_id'],
+                        $result[] = [
+                            'id_scrim' => $table[0]['scrims_id'],
                             'round' => $round,
                             'team1' => $table[0]['team_name'],
                             'team2' => '',
@@ -888,8 +886,8 @@ class ScrimMatchController extends Controller
                         ];
                         $winner=0;
                     } else  {
-                        $participans[] = [
-                            // 'id_scrim' => $table[0]['scrims_id'],
+                        $result[] = [
+                            'id_scrim' => $table[0]['scrims_id'],
                             'round' => $round,
                             'team1' => $table[0]['team_name'],
                             'team2' => $table[1]['team_name'],
@@ -897,13 +895,13 @@ class ScrimMatchController extends Controller
                         ];
                         $winner=rand(0,1);    // Generate a winner
                     }
-                    $participans[]=$table[$winner];  // Add WInnerto next round
+                    $teamMatch[]=$table[$winner];  // Add WInnerto next round
                 }
             }
             return response()->json([
                 'status' => 'success',
                 'message' => 'Team match found',
-                'data' => $participans,
+                'data' => $result,
             ], 200);
         } catch (\Exception $e) {
             return response()->json([
